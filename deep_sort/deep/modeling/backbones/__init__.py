9ea7fdb4cf31ec600c43c5072f82d792ba997b9a
GlassyWing <1490215053@qq.com>
2020-08-05 15:17:18 +0800

    add more backbone

--- deep_sort/deep/modeling/backbones/__init__.py
+++ deep_sort/deep/modeling/backbones/__init__.py
@@ -7,3 +7,5 @@
 from .build import build_backbone, BACKBONE_REGISTRY
 
 from .resnet import build_resnet_backbone
+from .darknet import build_darknet_backbone
+from .custom import build_custom_backbone
